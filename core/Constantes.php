a5f0ecca8e6bfa552080c8eab5a6a9d8cafbb604
WebSiteMaker <50154980+WebSiteMaker24@users.noreply.github.com>
2026-03-28 14:31:19 +0100

    Constantes.php

--- core/Constantes.php
+++ core/Constantes.php
@@ -1,77 +1,59 @@
 <?php
-// constantes.php
-// Utilisation de constante pour les routes et informations de votre entreprise afin de centraliser ces données
+// Constantes.php
 
-// Url prefix
-if (!defined('URL_PREFIX')) // "?url=" en local, et "/" en prod !!
-    define('URL_PREFIX', '?url=');
+// --------------------------------------------------
+// Paramètres utiles
+// --------------------------------------------------
+const URL_PREFIX = "?url=";
+const URL_ROOT = __DIR__;
 
-// Navigation
-if (!defined('URL_ACCUEIL'))
-    define('URL_ACCUEIL', "/");
-if (!defined('URL_SERVICE'))
-    define('URL_SERVICE', URL_PREFIX . 'service');
-
-if (!defined('URL_CONTACT'))
-    define('URL_CONTACT', URL_PREFIX . 'contact');
+// --------------------------------------------------
+// Liens des pages
+// --------------------------------------------------
+const URL_ACCUEIL = URL_PREFIX . "";
+const URL_CONTACT = URL_PREFIX . "";
 
 // 404
-if (!defined('URL_404'))
-    define('URL_404', URL_PREFIX . '404');
+const URL_404 = URL_PREFIX . '404';
 
-// Autres liens (réseaux sociaux ou autres)
-if (!defined('URL_FACEBOOK'))
-    define('URL_FACEBOOK', '#');
-if (!defined('URL_GOOGLE'))
-    define('URL_GOOGLE', '#');
-if (!defined('URL_TWITTER'))
-    define('URL_TWITTER', '#');
-if (!defined('URL_INSTAGRAM'))
-    define('URL_INSTAGRAM', '#');
+// --------------------------------------------------
+// Réseaux sociaux
+// --------------------------------------------------
+const URL_FACEBOOK = '#';
+const URL_GOOGLE = '#';
+const URL_TWITTER = '#';
+const URL_INSTAGRAM = '#';
 
+// --------------------------------------------------
 // Domaine
-if (!defined('URL_DOMAINE'))
-    define('URL_DOMAINE', 'https://votre_domaine.fr');
-if (!defined('URL_DOMAINE_TXT'))
-    define('URL_DOMAINE_TXT', 'votre_domaine.fr');
+// --------------------------------------------------
+const URL_DOMAINE = 'https://votre_domaine.fr';
+const URL_DOMAINE_TXT = 'votre_domaine.fr';
 
+// --------------------------------------------------
+// Informations entreprise
+// --------------------------------------------------
+const COMPANY_EMAIL = '';
+const COMPANY_EMAIL_SMTP = '';
+const COMPANY_NAME = 'PepinPHP';
+const COMPANY_PHONE = '';
+const COMPANY_PHONE_LINK = '';
+const COMPANY_ADDRESS = '';
+const COMPANY_SLOGAN = 'Framework PHP Maison';
+const COMPANY_PRICE_HOUR = '';
 
-// Information entreprise
-if (!defined('COMPANY_EMAIL'))
-    define('COMPANY_EMAIL', '');
-if (!defined('COMPANY_EMAIL_SMTP'))
-    define('COMPANY_EMAIL_SMTP', '');
+// --------------------------------------------------
+// Informations géographiques (balises meta)
+// --------------------------------------------------
+const GEO_PLACENAME = '';
+const GEO_CODE_POSTAL = '';
+const GEO_REGION = '';
+const GEO_POSITION = '';
+const GEO_POSITION_LATITUDE = '';
+const GEO_POSITION_LONGITUDE = '';
 
-if (!defined('COMPANY_NAME'))
-    define('COMPANY_NAME', 'PepinPHP');
-if (!defined('COMPANY_PHONE'))
-    define('COMPANY_PHONE', '');
-if (!defined('COMPANY_PHONE_LINK'))
-    define('COMPANY_PHONE_LINK', '');
-if (!defined('COMPANY_ADDRESS'))
-    define('COMPANY_ADDRESS', '');
-if (!defined('COMPANY_SLOGAN'))
-    define('COMPANY_SLOGAN', "Framework PHP Maison");
-if (!defined('COMPANY_PRICE_HOUR'))
-    define('COMPANY_PRICE_HOUR', "");
-
-// Geo information (balise meta)
-if (!defined('GEO_PLACENAME'))
-    define('GEO_PLACENAME', '');
-if (!defined('GEO_CODE_POSTAL'))
-    define('GEO_CODE_POSTAL', '');
-if (!defined('GEO_REGION'))
-    define('GEO_REGION', '');
-if (!defined('GEO_POSITION'))
-    define('GEO_POSITION', '');
-if (!defined('GEO_POSITION_LATITUDE'))
-    define('GEO_POSITION_LATITUDE', '');
-if (!defined('GEO_POSITION_LONGITUDE'))
-    define('GEO_POSITION_LONGITUDE', '');
-
+// --------------------------------------------------
 // Images
-if (!defined('IMG_LOGO'))
-    define('IMG_LOGO', 'public/img/logo.png');
-if (!defined('IMG_OG'))
-    define('IMG_OG', 'public/img/og_img.webp');
-
+// --------------------------------------------------
+const IMG_LOGO = 'public/img/logo.png';
+const IMG_OG = 'public/img/og_img.webp';
